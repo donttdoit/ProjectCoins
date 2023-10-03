e8890275e6c016f521ba79e5fc4e5a7f9aa64aa6
Maxim Merkulov <map20i0253@gmail.com>
2023-10-03 19:13:48 +0300

    Added player config

---
+++ Assets/Scripts/Configs/PlayerConfig.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerConfig", menuName = "Configs/PlayerConfig")]
+public class PlayerConfig : ScriptableObject
+{
+    [SerializeField] public float Speed;
+}
