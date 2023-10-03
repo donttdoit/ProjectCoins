5baa3b0e37f4912a0e025fe112885282c2ad7259
Maxim Merkulov <map20i0253@gmail.com>
2023-10-03 19:20:31 +0300

    rewrite installers in gamelevelInstaller

--- Assets/Scripts/Installers/CoinSpawnerInstaller.cs
+++
@@ -1,9 +0,0 @@
-using Zenject;
-
-public class CoinSpawnerInstaller : MonoInstaller
-{
-    public override void InstallBindings()
-    {
-        Container.Bind<CoinFactory>().AsSingle();
-    }
-}
