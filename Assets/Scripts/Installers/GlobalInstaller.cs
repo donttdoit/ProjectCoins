559dd4ff2f9ca9e292a085e1f3152106bbe70d68
Maxim Merkulov <map20i0253@gmail.com>
2023-10-03 19:22:19 +0300

    InputHandler now in GlobalInstaller

--- Assets/Scripts/Installers/GlobalInstaller.cs
+++ Assets/Scripts/Installers/GlobalInstaller.cs
@@ -1,10 +1,17 @@
+using System;
 using Zenject;
 
 public class GlobalInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
+        BindInput();
         BindResources();
+    }
+
+    private void BindInput()
+    {
+        Container.BindInterfacesAndSelfTo<InputHandler>().AsSingle();
     }
 
     private void BindResources()
