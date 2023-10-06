2ca42305da3b6b29ba541a8739b24b2fd9ddf31b
Maxim Merkulov <map20i0253@gmail.com>
2023-10-06 20:51:42 +0300

    fixing twitching movement with mouse

--- Assets/Scripts/Movement/MouseMovePattern.cs
+++ Assets/Scripts/Movement/MouseMovePattern.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class MouseMovePattern : IMovementHandler, IDisposable
 {
-    private const float MinDistanceToMove = 0.1f;
     private IMover _mover;
     private InputHandler _inputHandler;
     private bool _IsMouseOverMover;
@@ -24,22 +23,9 @@ public class MouseMovePattern : IMovementHandler, IDisposable
 
     public void UpdateMove()
     {
-        Debug.Log($"Forward: {_mover.Transform.up}");
-        //Debug.Log($"Player Position: {_mover.Transform.position}");
-        //Vector2 forwardBorderPoint = new Vector2(_mover.Transform.position.x + _mover.Transform.up.x * _mover.Collider.bounds.extents.x,
-        //                                         _mover.Transform.position.y + _mover.Transform.up.y * _mover.Collider.bounds.extents.y);
-
-        //Debug.Log($"BorderForwardPoint: {ConvertToWorldPosition(forwardBorderPoint)}");
-        //Debug.Log($"BorderForwardPoint: {forwardBorderPoint}");
-        //Debug.Log($"WorldMousePosition: {_inputHandler.GetWorldMousePosition()}");
-        Vector2 mousePosition = _inputHandler.GetWorldMousePosition();
-        //Debug.Log(_inputHandler.GetMousePosition());
-        //Vector2 
-        //Vector2 distanceBetweenCollider = new Vector2
-
         if (_inputHandler.IsMouseLeftButtonPressed() && _IsMouseOverMover == false)
         {
-            
+            Vector2 mousePosition = _inputHandler.GetWorldMousePosition();
             Vector2 direction = (mousePosition - new Vector2(_mover.Transform.position.x, _mover.Transform.position.y)).normalized;
             _mover.RigidBody.velocity = direction * _mover.Speed;
 
@@ -52,6 +38,4 @@ public class MouseMovePattern : IMovementHandler, IDisposable
 
     private void OnMouseOverMover() => _IsMouseOverMover = true;
     private void OnMouseExitMover() => _IsMouseOverMover = false;
-
-    private Vector2 ConvertToWorldPosition(Vector2 localPosition) => Camera.main.ScreenToWorldPoint(localPosition);
 }
