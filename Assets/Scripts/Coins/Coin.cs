6fe3cfb2d612dab30fd55ce6d12447296d5e02e5
Maxim Merkulov <map20i0253@gmail.com>
2023-10-09 20:50:46 +0300

    fixing trigger way

--- Assets/Scripts/Coins/Coin.cs
+++ Assets/Scripts/Coins/Coin.cs
@@ -6,18 +6,22 @@ public abstract class Coin : MonoBehaviour
     public Vector2 Position { get => transform.position; set => transform.position = value; }
 
     protected int Value { get; private set; } = 0;
-
     protected Wallet Wallet;
+    protected bool IsTriggered;
 
     [Inject]
-    private void Construct(Wallet wallet) { Wallet = wallet; }
+    private void Construct(Wallet wallet) => Wallet = wallet;
 
     public void Initialize(int value) => Value = value;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void Update()
     {
-        PickingCoin();
+        if (IsTriggered == true)
+            PickingCoin();
     }
+
+    private void OnTriggerEnter2D(Collider2D collision) => IsTriggered = true;
+    private void OnTriggerExit2D(Collider2D collision) => IsTriggered = false;
 
     protected abstract void PickingCoin();
 }
